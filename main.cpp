469cba1f550f06287dcf5f7cd413ec7a47182111
liemoren <1344041926@qq.com>
2024-07-17 11:44:41 +0800

    add new line

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
 std::cout<<"Fucking shit!"<<std::endl;
 std::cout<<"This is a new line!"<<std::endl;
 std::cout<<"Hello World!"<<std::endl;
+std::cout<<"This line is to show merge conflict.\n";
     return 0;
 }
