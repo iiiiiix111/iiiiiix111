b16ffb142d9f2c0c69bcf78ba935cdee24ce88b0
liemoren <1344041926@qq.com>
2024-07-17 11:21:11 +0800

    updated main.cpp with emergency-fix

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int main() {
-std::cout<<"Fucking shit!"<<std::endl;
+    
+    std::cout<<"This line is here to show the deletion."<<std::endl;
 std::cout<<"This is a new line!"<<std::endl;
 std::cout<<"Hello World!"<<std::endl;
+std::cout<<"This line is here to show how merging works."
+         <<std::endl;
     return 0;
 }
