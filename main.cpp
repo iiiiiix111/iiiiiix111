4074a5fb76f64f46b91b5c75e6a0bb971325b71b
猎魔人 <145643018+iiiiiix111@users.noreply.github.com>
2024-07-17 16:31:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     
     std::cout<<"This line is here to show the deletion."<<std::endl;
-std::cout<<"This is a new line!"<<std::endl;
+
 std::cout<<"Hello World!"<<std::endl;
 std::cout<<"This line is here to show how merging works."
          <<std::endl;
