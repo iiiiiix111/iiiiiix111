12e48856b8a3e45d8505236b43954b8ced238677
liemoren <1344041926@qq.com>
2024-09-09 09:33:01 +0800

    modified main.c

--- game3/main.c
+++ game3/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 char* toLowerCase(char* s) {
     char* p = s;
     while (*p != '\0') {
@@ -22,11 +23,36 @@ int addDigits(int num) {
     return n;
 
 }
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* shuffle(int* nums, int numsSize, int n, int* returnSize){
+    int* arr=(int*)malloc(numsSize*sizeof(int));
+    int i;
+    for ( i=0;i<n;i++)
+    {
+        arr[2*i]=nums[i];
+        arr[2*i+1]=nums[i+n];
+    }
+    *returnSize=numsSize;
+    return arr;
+}
+void show_array(int arr[],size_t s)
+{
+    for (size_t i=0;i<s;i++)
+        printf("%d ",arr[i]);
+
+}
 int main()
 {
     unsigned a=-2147483647-1U;
-    int num=123456;
-    printf("%d\n", addDigits(num));
-    printf("%u",a);
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int *return_size= (int*) malloc(sizeof(int));
+    int *new_arr= shuffle(arr,10,5,return_size);
+    show_array(new_arr,*return_size);
+    free(new_arr);
+    free(return_size);
     return 0;
 }
